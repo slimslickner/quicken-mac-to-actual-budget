a73e0278545e5d37266b03f6beaab916c66b14b7
Tim Tickner <timothy.tickner@gmail.com>
2024-11-06 22:48:25 -0500

    cleanup the csv manipulation file

--- src/csv_manipulation.py
+++ src/csv_manipulation.py
@@ -1,47 +1,52 @@
 # %%
+# Imports
 import csv
-import os
+from pathlib import Path
 import pandas as pd
 import yaml
+import logging
+
+# Setup logging
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+)
 
 # %%
 # Setup Reference Variables
-path_to_raw_csv = "data/raw_banking_export/"
-source_file = sorted(os.listdir(os.path.abspath(path_to_raw_csv)), reverse=True)[0]
-print(source_file)
-full_source_path = f"{path_to_raw_csv}/{source_file}"
-print(full_source_path)
-working_filepath = "data/working/working.csv"
+path_to_raw_csv = Path("data/raw_banking_export/")
+source_file = sorted(path_to_raw_csv.iterdir(), reverse=True)[0]
+logging.info(f"Source file: {source_file}")
+working_filepath = Path("data/working/working.csv")
 
-config_path = "config/config.yaml"
+config_path = Path("config/config.yaml")
 start_date = "2024-02-29"
 
 # %%
 # Read Config
-config = {}
 with open(config_path, "r") as f:
     config = yaml.safe_load(f)
+logging.info("Config file loaded")
 
 # %%
 # Reformat the exported transactions to only have meaningful data
-with open(full_source_path, "r") as f:
+with open(source_file, "r") as f:
     csv_reader = csv.reader(f)
-    with open(working_filepath, "w") as fw:
+    with open(working_filepath, "w", newline="") as fw:
         csv_writer = csv.writer(fw)
         writing = False
         for r in csv_reader:
             if "Account" in r:
                 writing = True
-
             if writing:
                 csv_writer.writerow(r[1:])
-
+logging.info("Reformatted transactions written to working CSV")
 
 # %%
 # Read in the working CSV that was just created
 df = pd.read_csv(working_filepath, na_values=["0", "NULL", "NA"])
+logging.info("Working CSV read into DataFrame")
 
-# rename headers
+# Rename headers
 df = df.rename(
     columns={
         "Date": "date",
@@ -55,40 +60,36 @@ df = df.rename(
         "Tags": "tags",
     }
 )
+logging.info("Headers renamed")
 
 # Convert Datatypes
-df["amount"] = df["amount"].str.replace(",", "")
-df["amount"] = pd.to_numeric(df["amount"])
+df["amount"] = df["amount"].str.replace(",", "").astype(float)
 df["date"] = pd.to_datetime(df["date"], format="mixed")
-
-# df.head()
+logging.info("Data types converted")
 
 # %%
 # Get config payee groups
 df_groupby_payees = pd.DataFrame(config["groupby_payees"])
-
 income_payees = df_groupby_payees[df_groupby_payees["category"] == "Income"][
     "payee"
 ].to_list()
+logging.info("Config payee groups loaded")
 
 # %%
 # Get only dates after 2/29/24
 df = df[df["date"] >= pd.to_datetime(start_date)]
-
-# df.sort_values(by=["date"], ascending=False).head(20)
+logging.info("Filtered transactions by date")
 
 # %%
 # Get only transactions from relevant accounts
 account_list = config["accounts"]
 df = df[df["account"].isin(account_list)]
-
-# df.head()
+logging.info("Filtered transactions by relevant accounts")
 
 # %%
 # Get only cleared transactions
 df = df[df["clr"] == "R"]
-
-# df.head()
+logging.info("Filtered cleared transactions")
 
 # %%
 # Clean data
@@ -98,28 +99,19 @@ df["payee"] = df["payee"].str.replace("{", " - ").str.replace("}", "")
 df["Split"] = df["Split"].fillna("")
 df["Scheduled"] = df["Scheduled"].fillna("")
 df["tags"] = df["tags"].fillna("")
-
-# df.head(20)
+logging.info("Cleaned data")
 
 # %%
 # Set the category to the last item in the split on :
 df["category"] = df["category"].str.split(":").str[-1]
-
-# df.head()
-
+logging.info("Set category to the last item in the split")
 
 # %%
 # Change tags from comma separated to hashtags
-def transform_tags(tags):
-    # Split the string into a list
-    tag_list = tags.split(", ")
-    # Prepend "#" to each tag and join back into a single string
-    transformed_tags = "".join([f"#{tag}" for tag in tag_list])
-    return transformed_tags
-
-
-# Apply the transformation to the "tags" column
-df["tags"] = df["tags"].apply(transform_tags)
+df["tags"] = df["tags"].apply(
+    lambda tags: "".join([f"#{tag}" for tag in tags.split(", ")])
+)
+logging.info("Transformed tags to hashtags")
 
 # %%
 # Add tags to the notes line
@@ -131,83 +123,42 @@ df["notes"] = df.apply(
     ),
     axis="columns",
 )
+logging.info("Added tags to notes")
 
 # %%
 # Update the groupby payees
 df = df.merge(df_groupby_payees, on="payee", how="left", suffixes=("", "_groupby"))
-# df_groupby_merge.head()
 df["category"] = df["category_groupby"].combine_first(df["category"])
 df["notes"] = df["notes_groupby"].combine_first(df["notes"])
 df["tags"] = df["tags_groupby"].combine_first(df["tags"])
-# df.head(40)
 df = df.drop(["category_groupby", "notes_groupby", "tags_groupby"], axis=1)
-
-# %%
-# Create a column to be used in split aggs
-# df["notes"] = df.apply(
-#     lambda x: (
-#         f"{x['category']} {x['amount']} ({x['notes']})"
-#         if x['Split'] == "S" and x['notes']
-#         else f"{x['category']} {x['amount']}"
-#         if x['Split'] == "S"
-#         else x['notes']
-#     ),
-#     axis="columns"
-# )
-
-# df.head(20)
-
-# %%
-# Set category for income payees
-# df["category"] = df.apply(
-#     lambda x: "Income" if x["payee"] in income_payees else x["category"],
-#     axis="columns"
-# )
-
-# # Set notes for income payees
-# df["notes"] = df.apply(
-#     lambda x: "Paycheck" if x["payee"] in income_payees else x["notes"],
-#     axis="columns"
-# )
-
-# df.head(20)
+logging.info("Updated groupby payees")
 
 # %%
 # Group by fitid to resolve Splits, this should really only merge income payees
 df = (
     df.groupby(["date", "account", "payee", "category", "notes", "fitid"])
-    .agg(
-        {
-            # "notes": "|".join,
-            "amount": "sum"
-        }
-    )
+    .agg({"amount": "sum"})
     .reset_index()
     .round(2)
 )
-
-# df.head(20)
-# df.sort_values(by=["date"], ascending=False).head(50)
-
+logging.info("Grouped by fitid to resolve splits")
 
 # %%
 # Write the new full dataset to CSV
-print(f"{os.path.abspath('data/processed/')}/full.csv")
-df.to_csv(f"{os.path.abspath('data/processed/')}/full.csv", index=False)
+output_dir = Path("data/processed/")
+output_dir.mkdir(parents=True, exist_ok=True)
+df.to_csv(output_dir / "full.csv", index=False)
+logging.info("Full dataset written to CSV")
 
 # Write individual files out
 for account in df["account"].unique():
-    print(account)
     new_df = df[df["account"] == account]
     final_df = new_df[["date", "payee", "notes", "category", "amount"]]
-    final_df.to_csv(
-        f"{os.path.abspath('data/processed/')}/{account}.csv".replace(" ", "").replace(
-            "'", ""
-        ),
-        index=False,
-    )
+    stripped_account = account.replace(" ", "").replace("'", "")
+    final_df.to_csv(output_dir / f"{stripped_account}.csv", index=False)
+    logging.info(f"Processed account: {account}")
 
-print()
-print(f"Processed {source_file}")
+logging.info(f"Processed {source_file}")
 
 # %%
